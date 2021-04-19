5ffc99d803fbd608f3745d5a2f5902fa3d18e563
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2021-04-19 13:37:49 -0500

    final app.py

--- app.py
+++ app.py
@@ -56,3 +56,81 @@ def precipitation():
 
     session.close()
 
+
+  
+    
+    # Convert the list to Dictionary
+    all_prcpt = []
+    for date,prcp  in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+               
+        all_prcpt.append(prcp_dict)
+
+    return jsonify(all_prcpt)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all Stations"""
+    # query all Stations
+    results = session.query(Station.station).\
+                 order_by(Station.station).all()
+
+    session.close()
+
+    # convert list of tuples into normal list
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # create session (link) from Python to the DB
+    session = Session(engine)
+  
+
+   # query tobs
+
+    results = session.query(Measurement.date,  Measurement.tobs,Measurement.prcp).\
+                filter(Measurement.date >= '2016-08-23').\
+                filter(Measurement.station=='USC00519281').\
+                order_by(Measurement.date).all()
+
+    session.close()
+
+
+
+    # create start route
+@app.route("/api/v1.0/min_max_avg/<start>")
+def start(start):
+    
+    # create session link
+    session = Session(engine)
+
+    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date."""
+
+    # take any date and convert to yyyy-mm-dd format for the query
+    start_dt = dt.datetime.strptime(start, '%Y-%m-%d')
+
+    # query data for the start date value
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_dt).all()
+
+    session.close()
+
+    # Create a list to hold results
+    t_list = []
+    for result in results:
+        r = {}
+        r["StartDate"] = start_dt
+        r["TMIN"] = result[0]
+        r["TAVG"] = result[1]
+        r["TMAX"] = result[2]
+        t_list.append(r)
+
+    # jsonify the result
+    return jsonify(t_list)
